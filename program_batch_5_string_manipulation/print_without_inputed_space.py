07b94660826dccaef7aa60c6d8fc88c57dadde93
unknown <carlosroque117@gmail.com>
2025-03-20 07:18:48 +0800

    Made a code aka .strip to remove spaces added before the name by the user.

--- program_batch_5_string_manipulation/print_without_inputed_space.py
+++ program_batch_5_string_manipulation/print_without_inputed_space.py
@@ -2,5 +2,6 @@
 #Make a code that removes the following spaces of the entered name
 #Print the inputed name without space
 
-
-
+your_name = input("Please enter your name with spaces: ")
+remove_space = your_name.strip()
+print("This is your name without spaces:", remove_space)
