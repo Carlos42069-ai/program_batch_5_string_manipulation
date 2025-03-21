cef2226db9fc143353623649acb6f82f4c7e9f17
unknown <carlosroque117@gmail.com>
2025-03-22 05:05:46 +0800

    Corrected the spelling of caseing to casing in the pseudocode.

--- program_batch_5_string_manipulation/fullname_in_pascal_case.py
+++ program_batch_5_string_manipulation/fullname_in_pascal_case.py
@@ -1,5 +1,5 @@
 #Ask the user for a name.
-#Prints out in pascal caseing. 
+#Prints out in pascal casing. 
 
 asking_name = input("Please enter your name in incorrect casing: ")
 pascal_casing = asking_name.title().replace(" ", "")
