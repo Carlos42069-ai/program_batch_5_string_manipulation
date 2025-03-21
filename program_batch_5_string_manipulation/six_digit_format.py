2613ea347960e8140b68b8f74a3898f1e6dca310
unknown <carlosroque117@gmail.com>
2025-03-22 03:53:42 +0800

    Made a if else statement with a condition that prints out a number from 1-1000 only and filled the remaining place value with 0 to complete the 6 digit.

--- program_batch_5_string_manipulation/six_digit_format.py
+++ program_batch_5_string_manipulation/six_digit_format.py
@@ -3,3 +3,9 @@
 #Print in a 6 digit format
 
 enter_num = int(input("Please enter a number from (0-1000): "))
+if 0 <= enter_num <= 1000:
+    add_zero = f"{enter_num:06d}"
+    print("The number is: ", add_zero)
+
+else:
+    print("Please enter a valid input")
