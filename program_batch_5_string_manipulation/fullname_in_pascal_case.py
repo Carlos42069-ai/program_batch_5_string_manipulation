b0ffb175081759680734246c8415faa192e897dc
unknown <carlosroque117@gmail.com>
2025-03-22 05:01:26 +0800

    Created a program where it asks the user for a fullname in improper casing, then when combining the title() function and replace() funtion it turn into aproper cased pascal casing.

---
+++ program_batch_5_string_manipulation/fullname_in_pascal_case.py
@@ -0,0 +1,6 @@
+#Ask the user for a name.
+#Prints out in pascal caseing. 
+
+asking_name = input("Please enter your name in incorrect casing: ")
+pascal_casing = asking_name.title().replace(" ", "")
+print("The fullname inpute in improper casing is now: ", pascal_casing)
