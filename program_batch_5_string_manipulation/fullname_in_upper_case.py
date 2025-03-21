3a3e92ed66fa9af6419fa3af0adfba62fb3f7f09
unknown <carlosroque117@gmail.com>
2025-03-22 04:03:02 +0800

    Added a line of code where it prints your name in upper cases using the upper() str function.

--- program_batch_5_string_manipulation/fullname_in_upper_case.py
+++ program_batch_5_string_manipulation/fullname_in_upper_case.py
@@ -1,4 +1,5 @@
-#Ask user for their name
-#Print it in uppper case 
+#Ask user for their name.
+#Print it in upper case.
 
 asking_name = input("Please enter your name: ")
+upper_case_name = print("Your name in capital letters is: ", asking_name.upper())
