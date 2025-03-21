b90451906ae69a864cec6c225698b56cf1dcc5c2
unknown <carlosroque117@gmail.com>
2025-03-22 04:52:09 +0800

    Successfully made a program where it asks for a name, using the len() function it counts the characters that is input then prints the total counted character.

--- program_batch_5_string_manipulation/fullname_character_count.py
+++ program_batch_5_string_manipulation/fullname_character_count.py
@@ -1,2 +1,6 @@
 #Ask the user for a name.
 #Print the count of characters inputed by the user.
+
+asking_name = input("Please enter your name: ")
+character_counter = len(asking_name) #the len function counts the characters of the inputed name.
+print("The total characters that you have input is: ", character_counter)
