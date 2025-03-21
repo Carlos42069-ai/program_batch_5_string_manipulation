440ae352886616b3350f987d8818b93a2200d958
unknown <carlosroque117@gmail.com>
2025-03-22 04:27:16 +0800

    It has the same idea as the previous three programs where we ask the name for improper casing then using the swapcase() function, it will swap the cases.

--- program_batch_5_string_manipulation/fullname_in_reverse_casing.py
+++ program_batch_5_string_manipulation/fullname_in_reverse_casing.py
@@ -1,3 +1,5 @@
 #Ask user for their full name.
 #Print it with a reversed casing.
 
+asking_name = input("Please enter your name in incorrect casing: ")
+reverse_casing = print("Your name in reversed casing is: ", asking_name.swapcase())
