1f56b9ebb9d97407c5531771dc163a5e8d68574d
unknown <carlosroque117@gmail.com>
2025-03-22 04:12:26 +0800

    Same idea as the previous program, added a code that ask the user for their name and prints it out in lower case using the lower() str function.

--- program_batch_5_string_manipulation/fullname_in_lower_case.py
+++ program_batch_5_string_manipulation/fullname_in_lower_case.py
@@ -1,2 +1,5 @@
 #Ask the user for their name.
 #Print the name in lower case.
+
+asking_name = input("Please enter your name: ")
+lower_case_name = print("Your name in lower case is: ", asking_name.lower())
