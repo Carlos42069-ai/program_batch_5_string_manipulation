1d4eae95b0c7d96ead47bd793d0bc31726b3e1c3
unknown <carlosroque117@gmail.com>
2025-03-22 04:19:59 +0800

    Same idea as the two previous program, asking the user for their name but this time ask to put it in improper casing. By using the title() str function, it will put it in a proper casing.

--- program_batch_5_string_manipulation/fullname_in_proper_casing.py
+++ program_batch_5_string_manipulation/fullname_in_proper_casing.py
@@ -1,3 +1,5 @@
 #Ask the user for their full name.
 #Print it out in proper casing.
 
+asking_name = input("Please enter your name in incorrect casing: ")
+proper_casing = print("Your full name in proper casing: ", asking_name.title())
