7b0331f6f905149e61f27efab618f5f29e7c444f
unknown <carlosroque117@gmail.com>
2025-03-22 05:13:32 +0800

    Successfully finishing batch 5 with this last program after 2 days (delayed because of projects and quizzes), so basically I combined the functions' lower() and replace() where function make any letters in lower case and replace to replace spaces to underscores turning it into a snake case.

--- program_batch_5_string_manipulation/fullname_in_snake_case.py
+++ program_batch_5_string_manipulation/fullname_in_snake_case.py
@@ -1,3 +1,6 @@
 #Ask the user for an improper name.
 #Prints out in snake case.
 
+asking_name = input("Please enter your name in incorrect casing: ")
+snake_casing = asking_name.lower().replace(" ", "_")
+print("The name that you entered is now in snake case formatting: ", snake_casing)
